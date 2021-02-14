2f54a7816ff4214800789a01bb339de62812d3ac
Shane Keenan <shanewilliamkeenan@gmail.com>
2021-02-14 10:48:28 +0800

    sorting stats (abandoned for now)

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,66 @@ import styles from '../styles/Home.module.css'
 
 export default function Home({pokemon}) {
 
+  // const getWinners = () =>{
+  //   var data = [...pokemon]
+  //   var top5Attack = data.sort(function(a, b) { return a.attack < b.attack ? 1 : -1; })
+  //               .slice(0, 5);
+  //   top5Attack.map((winner, index) =>{
+  //     index = index + 1;
+  //     let poke = pokemon.find(x => x.id === winner.id);
+  //     poke.attackPosition = index;
+  //   })
+  //   var data = [...pokemon]
+  //   var top5Defense = data.sort(function(a, b) { return a.defense < b.defense ? 1 : -1; })
+  //               .slice(0, 5);
+  //   top5Defense.map((winner, index) =>{
+  //     index = index + 1;
+  //     let poke = pokemon.find(x => x.id === winner.id);
+  //     poke.defensePosition = index;
+  //   })
+  //   var data = [...pokemon]
+  //   var top5HP = data.sort(function(a, b) { return a.hp < b.hp ? 1 : -1; })
+  //               .slice(0, 5);
+  //   top5HP.map((winner, index) =>{
+  //     index = index + 1;
+  //     let poke = pokemon.find(x => x.id === winner.id);
+  //     poke.hpPosition = index;
+  //   })
+  //   var data = [...pokemon]
+  //   var top5Speed = data.sort(function(a, b) { return a.speed < b.speed ? 1 : -1; })
+  //               .slice(0, 5);
+  //   top5Speed.map((winner, index) =>{
+  //     index = index + 1;
+  //     let poke = pokemon.find(x => x.id === winner.id);
+  //     poke.speedPosition = index;
+  //   })
+  //   var data = [...pokemon]
+  //   var top5SpecialAttack = data.sort(function(a, b) { return a.['special-attack'] < b.['special-attack'] ? 1 : -1; })
+  //               .slice(0, 5);
+  //   top5SpecialAttack.map((winner, index) =>{
+  //     index = index + 1;
+  //     let poke = pokemon.find(x => x.id === winner.id);
+  //     poke.specialAttackPosition = index;
+  //   })
+  //   var data = [...pokemon]
+  //   var top5SpecialDefense = data.sort(function(a, b) { return a.['special-defense'] < b.['special-defense'] ? 1 : -1; })
+  //               .slice(0, 5);
+  //   top5SpecialDefense.map((winner, index) =>{
+  //     index = index + 1;
+  //     let poke = pokemon.find(x => x.id === winner.id);
+  //     poke.specialDefensePosition = index;
+  //   })
+  //   // var data = [...pokemon]
+  //   // var data = data.filter(pokeman => pokeman.type.includes('fire'))
+  //   // var top5SpecialDefenseFire = data.sort(function(a, b) { return a.['special-defense'] < b.['special-defense'] ? 1 : -1; })
+  //   //             .slice(0, 5);
+  //   // top5SpecialDefenseFire.map((winner, index) =>{
+  //   //   index = index + 1;
+  //   //   let poke = pokemon.find(x => x.id === winner.id);
+  //   //   poke.specialDefensePositionFire = index;
+  //   // })
+  // }
+
   const [keyword, setKeyword] = useState('')
 
   const filteredPokemon = pokemon.filter(
@@ -18,6 +78,7 @@ export default function Home({pokemon}) {
 
     setKeyword(e.target.value.toLowerCase());
   }
+  // getWinners();
 
   return (
     <Layout>
@@ -37,7 +98,8 @@ export default function Home({pokemon}) {
 export const getStaticProps = async () =>{
   const promises = []
   const pokemon = []
-  for(let i =1; i < 899; i++){
+
+  for(let i =1; i < 50; i++){
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then((res) => res.json()));
   }
@@ -58,7 +120,7 @@ export const getStaticProps = async () =>{
             smallImage: data.sprites['front_default'],
             type: data.types.map(type => type.type.name).join(', ')
           }))
-          pokemon.push(poke)
+          pokemon.push(poke)          
   })
   return{
     props: {
